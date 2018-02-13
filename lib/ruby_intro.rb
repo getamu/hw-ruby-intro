30c2193f503ed326e69d10f689b9a5a75b439f64
getamu <amulya123@tamu.edu>
2018-02-13 04:12:39 +0000

    hw complete

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -2,34 +2,81 @@
 
 # Part 1
 
-def sum arr
-  # YOUR CODE HERE
+def sum arr 
+  sum=0
+    arr.each do|i|
+      sum+=i
+    end
+  return sum;
 end
 
 def max_2_sum arr
-  # YOUR CODE HERE
+  if arr.empty? 
+    return 0
+  end
+  if arr.length==1 
+    result=arr[0]
+  else
+    arr.sort!{|x,y| y<=>x}
+    result=arr[0]+arr[1]
+  end
+  return result
 end
 
 def sum_to_n? arr, n
-  # YOUR CODE HERE
+  if arr.empty? or arr.length==1 
+    return false
+  end
+  hash=Hash.new
+  arr.each{|val| 
+            if hash.key? val 
+              return true
+            else
+              hash[n-val]=n
+            end
+  }
+  return false
+    
 end
 
 # Part 2
 
 def hello(name)
-  # YOUR CODE HERE
+  return "Hello, "+name
 end
 
 def starts_with_consonant? s
-  # YOUR CODE HERE
+  if /^[^aeiou\W]/i.match(s) == nil
+    return false
+  else
+    return true
+  end
 end
 
 def binary_multiple_of_4? s
-  # YOUR CODE HERE
+  if s == "0"
+    return true
+  end
+  if /^[01]*(00)$/.match(s) #|| /^0$/.match(s)
+    return true
+  else
+    return false
+  end
 end
 
 # Part 3
 
 class BookInStock
-# YOUR CODE HERE
+  attr_accessor :isbn,:price
+  def initialize(num,val)
+    if num.empty? or val<=0
+      raise ArgumentError
+    end
+    @isbn=num
+    @price=val
+  end
+  def price_as_string
+    val= "$%.2f" % @price
+    return val
+  end
 end
